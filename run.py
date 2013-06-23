c2bd96441694a322f253b4cd3ddd1dbc72a0ec5a
trisha <kotharitrisha@gmail.com>
2013-06-23 19:07:17 -0400

    added from_no

--- run.py
+++ run.py
@@ -10,8 +10,11 @@ auth_token="fe9db2fafd17b354f0885cef8c8f043c"
 @app.route("/twilio", methods=['GET','POST'])
 def hello():
     client=TwilioRestClient(account_sid, auth_token)
-    client.sms.messages.create(from_="+14083296276", to="+15126668669",
-                               body= "lets say")
+    from_number=request.values.get('From', None)
+    body=request.values.get('Body', None)
+    parse(client, body, from_number)
+#    client.sms.messages.create(from_="+14083296276", to="+15126668669",
+#                               body= "lets say")
 
 if __name__=="__main__":
     app.run(debug=True)
@@ -21,9 +24,9 @@ if __name__=="__main__":
 #message = client.sms.messages.create(from_="+14083296276", to="+15126668669",
 #                                     body= "You have verified")
 
-def send_records(client):
+def send_records(client, from_no):
     records="Last 3 medical records: "
-    message_from = client.sms.messages.list(from_="=15126668669")
+    message_from = client.sms.messages.list(from_=from_no)
 
     y = 0
 
@@ -33,19 +36,19 @@ def send_records(client):
         if y==3:
             break
     print records
-    client.sms.messages.create(from_="+14083296276", to="+15126668669",
+    client.sms.messages.create(from_="+14083296276", to=from_no,
                                body= records)
 
-def update(client):
-    client.sms.messages.create(from_="+14083296276", to="+15126668669",
+def update(client, from_no):
+    client.sms.messages.create(from_="+14083296276", to=from_no,
                                body= "Thanks You for updating")
 
-def parse(client, message):
+def parse(client, message, from_no):
     splitter=message.split(" ")
     if splitter[0] == "update":
-        update(client)
+        update(client, from_no)
     else:
-        send_records(client)
+        send_records(client, from_no)
 
 #client=TwilioRestClient(account_sid, auth_token)
 #parse(client, msg)
