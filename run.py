87d6599e15781e235a05f252cc51e2fb14428544
trisha <kotharitrisha@gmail.com>
2013-06-23 16:22:37 -0400

    adding twilio

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask, request, redirect
-#import twilio.twiml
+import twilio.twiml
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def hello():
     print request.values.get('From', None)
-   # resp=twilio.twiml.Response()
-   # resp.sms("Hello, Mobile Monkey")
-   # return str(resp)
+    resp=twilio.twiml.Response()
+    resp.sms("Hello, Mobile Monkey")
+    return str(resp)
 
 if __name__=="__main__":
     app.run(debug=True)
